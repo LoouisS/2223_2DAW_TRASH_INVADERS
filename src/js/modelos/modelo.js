52e867fc6977ec9aef2df906b542d253f1adfb20
Nacarin <146434913+Nacarin@users.noreply.github.com>
2023-11-17 16:49:57 +0100

    Ultima subida

--- src/js/modelos/modelo.js
+++ src/js/modelos/modelo.js
@@ -1,14 +1,37 @@
-// modelos/modelo.js
+/**
+ * Clase que representa un modelo básico con funcionalidades de almacenamiento.
+ * @class
+ */
 export class Modelo {
-    constructor() {
-        this.mapa = new Map();
-    }
+  /**
+     * Crea una instancia de la clase Modelo.
+     * @constructor
+     */
+  constructor () {
+    /**
+         * Un mapa para almacenar pares clave-valor.
+         * @type {Map}
+         * @private
+         */
+    this.mapa = new Map()
+  }
 
-    guardar(clave, valor) {
-        this.mapa.set(clave, valor);
-    }
+  /**
+     * Guarda un valor asociado a una clave en el mapa.
+     * @param {string} clave - La clave para asociar al valor.
+     * @param {*} valor - El valor a guardar.
+     * @returns {void}
+     */
+  guardar (clave, valor) {
+    this.mapa.set(clave, valor)
+  }
 
-    ver(clave) {
-        return this.mapa.get(clave);
-    }
+  /**
+     * Obtiene el valor asociado a una clave del mapa.
+     * @param {string} clave - La clave para buscar el valor.
+     * @returns {*} El valor asociado a la clave, o undefined si no se encuentra.
+     */
+  ver (clave) {
+    return this.mapa.get(clave)
+  }
 }
--- src/js/vistas/vista.js
+++ src/js/vistas/vista.js
@@ -1,27 +1,49 @@
-export class Vista{
+/**
+ * Clase base que representa una vista en la aplicación.
+ * @class
+ */
+export class Vista {
+  /**
+     * Enumeración de símbolos que representan diferentes vistas.
+     * @readonly
+     * @enum {Symbol}
+     */
+  static {
+    Vista.VISTA1 = Symbol('Inicio')
+    Vista.VISTA2 = Symbol('Juego')
+    Vista.VISTA3 = Symbol('Ranking')
+  }
 
-    static {
-        Vista.VISTA1 = Symbol('Inicio')
-        Vista.VISTA2 = Symbol('Juego')
-        Vista.VISTA3 = Symbol('Ranking')
-    }
-
-    constructor(controlador,base) {
-        this.controlador = controlador
-        this.base = base
-    }
+  /**
+     * Crea una instancia de la clase Vista.
+     * @constructor
+     * @param {Controlador} controlador - Instancia del controlador asociado a la vista.
+     * @param {HTMLElement} base - Elemento HTML que sirve como la base de la vista.
+     */
+  constructor (controlador, base) {
+    /**
+         * Instancia del controlador asociado a la vista.
+         * @type {Controlador}
+         */
+    this.controlador = controlador
 
     /**
-     * Muestra u oculta la vista
-     * @param ver {Boolean} Indica si la vista debe mostrarse (true) u ocultarse(false)
+         * Elemento HTML que sirve como la base de la vista.
+         * @type {HTMLElement}
+         */
+    this.base = base
+  }
+
+  /**
+     * Muestra u oculta la vista.
+     * @param {boolean} ver - Indica si la vista debe mostrarse (true) u ocultarse (false).
+     * @returns {void}
      */
-
-    mostrar (ver) {
-        if (ver) {
-            this.base.style.display='block'
-        } else {
-            this.base.style.display='none'
-        }
+  mostrar (ver) {
+    if (ver) {
+      this.base.style.display = 'block'
+    } else {
+      this.base.style.display = 'none'
     }
+  }
 }
-
