2130eade3e8810d30b729002bf635dfa2ef5a1c7
tonifp1729 <tonifp1729@gmail.com>
2023-11-17 12:27:44 +0100

    Primera subida de configuración, modificacion de sql y controladores

--- sql/insertarFilasTIDB.sql
+++ sql/insertarFilasTIDB.sql
@@ -2,8 +2,13 @@
 --Inserción de filas en base de datos Trash Invaders.
 
 --Inserción de usuario "especial" administrador.
-INSERT INTO usuario (nickname)
-VALUES ('Adm');
+
+
+$pswrd = password_hash($_post["pw"]);
+
+INSERT INTO usuario(nickname, pswrd)
+
+VALUES('Adm','Entraaqui123'); 
 
 INSERT INTO administrador (idAdmin)
 VALUES ('Adm');
@@ -12,3 +17,6 @@ VALUES ('Adm');
 /*Estos solo van a poder ser modificados por el usuario administrador*/
 INSERT INTO parametros(velocidad_basura, generacion_basura, tiempo_espera, prob_aparicion_mejora)
 VALUES (10,30,15,30);
+
+
+password_verify($_post["pw"], $fila["pw"]])
