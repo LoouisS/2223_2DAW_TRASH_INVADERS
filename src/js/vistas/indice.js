c86a251b019a01d3c948587984ba4b837cedb468
tonifp1729 <tonifp1729@gmail.com>
2023-11-22 09:12:10 +0100

    2ª revisión

--- src/js/vistas/indice.js
+++ src/js/vistas/indice.js
@@ -1,3 +1,23 @@
 //vistaIndice
 import { Vista } from './vista.js'
 
+export class Indice extends Vista{
+
+    constructor (controlador, base) {
+        super(controlador, base)
+        this.crearInterfaz()
+    }
+
+    crearInterfaz () {
+        // Botón JUGAR
+        this.botonJugar = this.base.querySelector('#boton-jugar')
+        this.botonJugar.onclick = this.pulsarBotonJugar.bind(this)
+    }
+
+    pulsarBotonJugar () {
+        // Manejar el evento del botón JUGAR
+        // console.log('Botón JUGAR pulsado');
+        // Cambiar a la Vista3
+        this.controlador.verVista(Juego.divvistaJuego) // Cambiado a Vista3.VISTA3
+    }
+}
