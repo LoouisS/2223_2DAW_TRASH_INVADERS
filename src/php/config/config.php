8be4698dd1ca35c07af197a29eb76d7b53bafc58
Antonio Manuel Figueroa Pinilla <tonifp1729@gmail.com>
2023-11-23 20:27:25 +0100

    Reinicio

--- src/php/config/config.php
+++
@@ -1,9 +0,0 @@
-<!-- Antonio Manuel Figueroa Pinilla -->
-<!-- Archivo de configuración PHP -->
-<?php
-    /* Datos de conexión a la base de datos */
-    define("HOST", "2daw.esvirgua.com");
-    define("TRASHINVADERSBD", "user2daw_BD1-05");
-    define("USUARIO", "user2daw_05");
-    define("PSWBD", "^ax;=Sc)X0rJ");
-?>
--- src/php/controladores/loginctrl.php
+++
@@ -1,9 +0,0 @@
-<!-- Antonio Manuel Figueroa Pinilla -->
-<?php
-    
-    if(!empty($_POST["ingresar"])){
-        if(empty($_POST["username"]) and empty($_POST["password"])){
-
-        }
-    }
-?>
