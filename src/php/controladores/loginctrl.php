d3283d15e7b1f4e4caedb3388fb106143ab548ea
Antonio Manuel Figueroa Pinilla <tonifp1729@gmail.com>
2023-11-23 20:29:06 +0100

    Todo restablecido en el sprint2c al estado inicial

---
+++ src/php/controladores/loginctrl.php
@@ -0,0 +1,9 @@
+<!-- Antonio Manuel Figueroa Pinilla -->
+<?php
+    
+    if(!empty($_POST["ingresar"])){
+        if(empty($_POST["username"]) and empty($_POST["password"])){
+
+        }
+    }
+?>
