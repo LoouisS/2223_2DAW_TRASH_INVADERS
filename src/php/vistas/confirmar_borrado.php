9661bea237d599baca7252a2a7bfb3ecef481896
Nacarin <nacar297@gmail.com>
2023-11-24 11:53:20 +0100

    cambios css vistasphp

--- src/php/vistas/confirmar_borrado.php
+++ src/php/vistas/confirmar_borrado.php
@@ -1,14 +1,22 @@
-
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Confirmación de Borrado</title>
-</head>
-<body>
-    <h1>Confirmación de Borrado</h1>
-    <p>¿Estás seguro de que deseas eliminar esta imagen?</p>
-    
-    <?php echo "<a href='index.php?controlador=Imagenes&action=ejecucionBorrado&idImagen=" . $_GET['idImagen'] . "'>Eliminar</a>"; ?>
-    <a href="index.php?controlador=Imagenes&action=mostrarImagen">Cancelar</a>
-</body>
+    <head>
+        <title>Confirmación de Borrado</title>
+        <link rel="stylesheet" href="src/css/new_style.css">
+    </head>
+    <body id="register-page">
+        <main id="login-page">
+            <div class="div-superior">
+                <h1>Confirmación de Borrado</h1>
+            </div>
+            <div>
+                <img src="src/img/gato.png" alt="Descripción imagen" id="ejemploimagen"/>
+            </div>
+            <h1 id="eliminarconfir">¿Estás seguro de que deseas eliminar esta imagen?</h1>              
+            <?php echo "<button><a href='index.php?controlador=Imagenes&action=ejecucionBorrado&idImagen=" . $_GET['idImagen'] . "'>Eliminar</a></button>"; ?>               
+            <div>
+                <button><a href="index.php?controlador=Imagenes&action=mostrarImagen">Cancelar</a></button>
+            </div>  
+        </main>        
+    </body>
 </html>
